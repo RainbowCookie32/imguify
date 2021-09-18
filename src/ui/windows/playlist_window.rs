347319768106507c494b87fe1f70ca87f93a5ef9
Matias Moran <matimoran03@gmail.com>
2021-09-18 01:12:49 -0300

    use tables for the playlist window

--- src/ui/windows/playlist_window.rs
+++ src/ui/windows/playlist_window.rs
@@ -4,52 +4,72 @@ use imgui::*;
 
 pub fn build(ui: &Ui, app_state: &mut AppState) {
     Window::new("Playlist").size([800.0, 500.0], Condition::FirstUseEver).build(ui, || {
-        ui.text("Songs");
-
         if let Some(plist) = app_state.playlist_data.as_ref() {
-            if let Ok(lock) = plist.entries_data().try_read() {
-                let data_len = lock.len();
-                let entries_len = plist.entries().len();
-
-                if data_len != entries_len {
-                    ui.same_line_with_pos(70.0);
-                    ui.text_colored([1.0, 0.1, 0.1, 1.0], format!("Loading songs: {}/{}", data_len, entries_len));
-                }
-
-                ui.separator();
-
-                ui.columns(5, "Columns?", true);
-
-                for entry in lock.iter() {
-                    ui.text(entry.title());
-                    ui.next_column();
-
-                    ui.text(entry.artist());
-                    ui.next_column();
-
-                    let seconds = entry.duration() / 1000;
-                    let minutes = seconds / 60;
-                    let seconds = seconds % 60;
-
-                    ui.text(format!("{}:{:02}", minutes, seconds));
-                    ui.next_column();
-
-                    if ui.button(format!("Play##{}", entry.id())) {
-                        if let Some(handler) = app_state.spotify_handler.as_mut() {
-                            app_state.player_state.show = true;
-                            handler.play_song_on_playlist(plist.id().to_base62(), entry.id());
-                        }
+            if let Ok(mut entries) = plist.entries_data().try_write() {
+                let token = ui.begin_table_header_with_flags(
+                    "Playlist Table",
+                    [
+                        TableColumnSetup::new("Title"),
+                        TableColumnSetup::new("Artist"),
+                        TableColumnSetup::new("Duration"),
+                        TableColumnSetup::new("Actions")
+                    ],
+                    TableFlags::RESIZABLE | TableFlags::SORTABLE | TableFlags::BORDERS
+                );
+                
+                if let Some(_t) = token {
+                    if let Some(data) = ui.table_sort_specs_mut() {
+                        data.conditional_sort(|specs| {
+                            if let Some(spec) = specs.iter().next() {
+                                if let Some(direction) = spec.sort_direction() {
+                                    match direction {
+                                        TableSortDirection::Ascending => match spec.column_idx() {
+                                            0 => entries.sort_by_key(|e| e.title().clone()),
+                                            1 => entries.sort_by_key(|e| e.artist().clone()),
+                                            2 => entries.sort_by_key(|e| *e.duration()),
+                                            _ => {}
+                                        }
+                                        TableSortDirection::Descending => match spec.column_idx() {
+                                            0 => entries.sort_by_key(|e| std::cmp::Reverse(e.title().clone())),
+                                            1 => entries.sort_by_key(|e| std::cmp::Reverse(e.artist().clone())),
+                                            2 => entries.sort_by_key(|e| std::cmp::Reverse(*e.duration())),
+                                            _ => {}
+                                        }
+                                    }
+                                }
+                            }
+                        });
                     }
 
-                    ui.next_column();
+                    for entry in entries.iter() {
+                        ui.table_next_column();
+                        ui.text(entry.title());
 
-                    if ui.button(format!("Remove##{}", entry.id())) {
-                        if let Some(handler) = app_state.spotify_handler.as_mut() {
-                            handler.remove_track_from_playlist(&plist.id().to_base62(), entry.id());
+                        ui.table_next_column();
+                        ui.text(entry.artist());
+    
+                        let seconds = entry.duration() / 1000;
+                        let minutes = seconds / 60;
+                        let seconds = seconds % 60;
+    
+                        ui.table_next_column();
+                        ui.text(format!("{}:{:02}", minutes, seconds));
+
+                        ui.table_next_column();
+                        if ui.button(format!("Play##{}", entry.id())) {
+                            if let Some(handler) = app_state.spotify_handler.as_mut() {
+                                app_state.player_state.show = true;
+                                handler.play_song_on_playlist(plist.id().to_base62(), entry.id());
+                            }
+                        }
+    
+                        ui.same_line();
+                        if ui.button(format!("Remove##{}", entry.id())) {
+                            if let Some(handler) = app_state.spotify_handler.as_mut() {
+                                handler.remove_track_from_playlist(&plist.id().to_base62(), entry.id());
+                            }
                         }
                     }
-
-                    ui.next_column();
                 }
             }
         }
