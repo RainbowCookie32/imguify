411900a41e954b190804633c8c2fe5e0a52dec8d
Matias Moran <matimoran03@gmail.com>
2021-10-07 19:02:10 -0300

    dbus: fuck metadata
    it's being too annoying in some refactoring I'm doing

--- src/spotify/dbus.rs
+++ src/spotify/dbus.rs
@@ -1,9 +1,7 @@
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryInto;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::Sender;
-use std::collections::HashMap;
 
-use zvariant::*;
 use zbus::{fdo, dbus_interface};
 
 use crate::spotify::player::{PlayerCommand, PlayerHandler};
@@ -88,58 +86,6 @@ impl MPRISHandler {
         true
     }
 
-    #[dbus_interface(property, name = "Metadata")]
-    fn metadata(&self) -> HashMap<String, Value> {
-        let mut map = HashMap::new();
-
-        if let Ok(lock) = self.player_handler.lock() {
-            if let Some(track) = lock.get_current_song() {
-                let title = Value::Str(Str::from(track.title().clone()));
-                let artist = Value::Array(Array::from(vec![track.artist().clone()]));
-                let track_id = Value::ObjectPath(ObjectPath::try_from("/org/mpris/MediaPlayer2/imguify").unwrap());
-                let track_length = Value::U64(*track.duration() as u64 * 1000);
-
-                map.insert(String::from("mpris:trackid"), track_id);
-                map.insert(String::from("mpris:length"), track_length);
-                map.insert(String::from("xesam:title"), title);
-                map.insert(String::from("xesam:artist"), artist);
-                map.insert(String::from("xesam:album"), Value::Str(Str::from("")));
-            }
-        }
-
-        map
-    }
-
-    pub fn update_metadata(&self) -> zbus::Result<()> {
-        let invalidated: Vec<String> = vec![];
-        let mut changed = HashMap::new();
-        let mut metadata_dict = Dict::new(Str::signature(), Value::signature());
-
-        if let Ok(lock) = self.player_handler.lock() {
-            if let Some(track) = lock.get_current_song() {
-                let title = Value::Value(Box::new(Value::Str(Str::from(track.title().clone()))));
-                let artist = Value::Value(Box::new(Value::Array(Array::from(vec![track.artist().clone()]))));
-                let track_id = Value::Value(Box::new(Value::ObjectPath(ObjectPath::try_from("/org/mpris/MediaPlayer2/imguify").unwrap())));
-                let track_length = Value::Value(Box::new(Value::U64(*track.duration() as u64 * 1000)));
-
-                metadata_dict.append(Value::Str(Str::from("mpris:trackid")), track_id).unwrap();
-                metadata_dict.append(Value::Str(Str::from("mpris:length")), track_length).unwrap();
-                metadata_dict.append(Value::Str(Str::from("xesam:title")), title).unwrap();
-                metadata_dict.append(Value::Str(Str::from("xesam:artist")), artist.clone()).unwrap();
-                metadata_dict.append(Value::Str(Str::from("xesam:albumArtist")), artist).unwrap();
-            }
-        }
-
-        changed.insert("Metadata", Value::from(metadata_dict));
-
-        zbus::ObjectServer::local_node_emit_signal(
-            None,
-            "org.freedesktop.DBus.Properties",
-            "PropertiesChanged",
-            &("org.mpris.MediaPlayer2.Player", changed, invalidated)
-        )
-    }
-
     #[dbus_interface(property, name = "CanGoNext")]
     fn can_go_next(&self) -> bool {
         true
@@ -188,9 +134,7 @@ pub fn init_connection(events_tx: Sender<PlayerCommand>, player_handler: Arc<Mut
                                 println!("{}", err);
                             }
 
-                            object_server.with(&"/org/mpris/MediaPlayer2".try_into().unwrap(), |i: &MPRISHandler| {
-                                i.update_metadata()
-                            }).unwrap();
+                            std::thread::sleep(std::time::Duration::from_millis(10));
                         }
                     }
                 }
